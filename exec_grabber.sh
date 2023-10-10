6c53221cfdea771d7f636591534ad00f6bcc5ab9
David Park <dp247@rocketmail.com>
2023-10-10 12:21:33 +0100

    Update needed dependencies

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python3 -m pip install requests
+python3 -m pip install unicodedata
+python3 -m pip install lxml
 
 python3 YouTubeLinkGrabber.py > ./youtube.m3u8
 
-echo M3U grabbed.
+echo M3U update complete.
