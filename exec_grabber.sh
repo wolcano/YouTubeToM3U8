6e3a7167ee17cc16be78c9671c3ffcb1263aa069
David Park <dp247@rocketmail.com>
2023-10-10 13:48:23 +0100

    Amend dependencies

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 -m pip install requests
-python3 -m pip install unicodedata
 python3 -m pip install lxml
+python3 -m pip install pytz
 
 python3 YouTubeLinkGrabber.py > ./youtube.m3u8
 
